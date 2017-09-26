a80689b898b072e7bcbd4f502a731a233fb1278d
Maxime Rouiller <angelzerosoft@gmail.com>
2017-09-26 11:16:48 -0400

    set vscode tasks. first steps for vuejs

--- src/VmSizeBrowser.Web/wwwroot/app/site.js
+++ src/VmSizeBrowser.Web/wwwroot/app/site.js
@@ -1,22 +1,22 @@
 ﻿const $ = require('jquery');
+const numeral = require('numeral');
 
 let app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello Vue!',
-        vmSizes: []
+        vmSizes: [],
+        selectedRegion: 'EastUS'
+
     },
     filters: {
         divideBy1024: function megabytesToGigabytes(data) {
-            var result = (data / 1024);
-            return result == result.toFixed(0) ? result : result.toFixed(2);
+            return numeral(data*1024*1024).format('0.00b');
         }
 
     },
     mounted: function () {
         let self = this;
-        $("#region").val('EastUS'); // set default initial value.
-        $.getJSON("/api/VmSize/region/" + $("#region").val()).then(function (data) {
+        $.getJSON("/api/VmSize/region/" + self.selectedRegion).then(function (data) {
             self.vmSizes = data;
         });
 
@@ -24,67 +24,3 @@ let app = new Vue({
 });
 
 module.exports = app;
-
-// $(document).ready(function () {
-//    $("#region").val('EastUS'); // set default initial value.
-//    $("#table").DataTable({
-//        "ajax": {
-//            "url": "/api/VmSize/region/" + $("#region").val(),
-//            "dataSrc": ""
-//        },
-//        "columns": [
-//            { "data": "friendlyName" },
-//            { "data": "tier" },
-//            { "data": "apiName" },
-//            { "data": "numberOfCores" },
-//            { "data": "memoryInMb", render: megabytesToGigabytes },
-//            { "data": "maxNICs" },
-//            { "data": "gpuCount" },
-//            { "data": "maxDataDiskCount" },
-//            { "data": "osDiskSizeInMb", render: megabytesToGigabytes },
-//            { "data": "resourceDiskSizeInMb", render: megabytesToGigabytes }
-//        ]
-//    });
-
-//    $("#region").on('input', function () {
-//        appInsights.trackEvent('regionChanged', { region: $(this).val() });
-//        $("#table").DataTable().ajax.url("/api/VmSize/region/" + $(this).val());
-//        $("#table").DataTable().ajax.reload();
-//    });
-//    $("#minimumVCore").on('input', function () {
-//        $("#selectedvCores").text($(this).val());
-
-//    });
-//    $("#minimumMemory").on('input', function () {
-//        $("#selectedMemory").text($(this).val());
-
-//    });
-
-//    $("#minimumVCore").on('change', function () {
-//        debugger;
-//        appInsights.trackEvent('vCoreChanged', { minVCores: $(this).val() });
-//        $("#table").DataTable().table().draw();
-//    });
-//    $("#minimumMemory").on('change', function () {
-//        debugger;
-//        appInsights.trackEvent('memoryChanged', { minMemory: $(this).val() });
-//        $("#table").DataTable().table().draw();
-//    });
-
-//    $.fn.dataTable.ext.search.push(
-//       function (settings, data, dataIndex) {
-//           var minMemory = parseInt($('#minimumMemory').val(), 0);
-//           var minVCore = parseInt($('#minimumVCore').val(), 0);
-//           var vcore = parseFloat(data[3]) || 0;
-//           var memory = parseFloat(data[4]) || 0;
-   
-//           if (minVCore <= vcore && minMemory <= memory) {
-//               return true;
-//           }
-//           return false;
-//       }
-//    );
-//    function megabytesToGigabytes(data) {
-//       return data / 1024;
-//    }
-// });
